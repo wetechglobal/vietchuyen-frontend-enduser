ea595035e5a5d180c35e1378bc49fff7bbaf2e4b
TruHuynh_DXC <hctru68@gmail.com>
2020-12-23 13:45:29 +0700

    custom layout

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 
 function App() {
   return (
     <Container>
-      {/* Stack the columns on mobile by making one full-width and the other half-width */}
       <Row>
-        <Col xs={12} md={8}>
-          xs=12 md=8
-    </Col>
-        <Col xs={6} md={4}>
-          xs=6 md=4
-    </Col>
+        <Col className="header">
+          HEADER - NAVI
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Row className="slider">
+            <Col md={12}>SLIDER</Col>
+            <Col md={12}>SLIDER</Col>
+            <Col md={12}>SLIDER</Col>
+            <Col md={12}>SLIDER</Col>
+          </Row>
+          <Row className="latest-new">
+            <Col md={4}>TIN TUC</Col>
+            <Col md={4}>TIN TUC</Col>
+            <Col md={4}>TIN TUC</Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="footer">
+          FOOTER
+        </Col>
       </Row>
 
-      {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-      <Row>
-        <Col xs={6} md={4}>
-          xs=6 md=4
-    </Col>
-        <Col xs={6} md={4}>
-          xs=6 md=4
-    </Col>
-        <Col xs={6} md={4}>
-          xs=6 md=4
-    </Col>
-      </Row>
-
-      {/* Columns are always 50% wide, on mobile and desktop */}
-      <Row>
-        <Col xs={6}>xs=6</Col>
-        <Col xs={6}>xs=6</Col>
-      </Row>
     </Container>
   );
 }
