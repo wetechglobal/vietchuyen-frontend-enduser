cea81822ffd13e42cb2c67ddf8c2267fbb22d9e1
MaiThanhMen <mtmen1995@gmail.com>
2020-12-27 14:43:41 +0700

    commit news page

--- src/features/news/index.jsx
+++ src/features/news/index.jsx
@@ -1,12 +1,18 @@
 
 import { Row, Col } from 'react-bootstrap';
+import NewsImage from './components/newsImage/';
+import NewsInfo from './components/newsInfo';
+import './style.scss';
 
 const News = () => {
 
     return (
         <Row>
             <Col className="news">
-                News Page
+                <NewsImage />
+                <Row className="news-info">
+                    <NewsInfo />
+                </Row>
             </Col>
         </Row>
     );
