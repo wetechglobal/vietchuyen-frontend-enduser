75ac5a54fbdc5dfcfd89e02f072df0ee38ede842
TruHuynh_DXC <hctru68@gmail.com>
2021-01-01 22:23:35 +0700

    update script

--- src/features/about/index.jsx
+++ src/features/about/index.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <Col lg={12} className="about">
                 <Row className="text-about">
                     <Col lg={{ span: 6, offset: 2 }}>
-                        <h1>THƯ NGỎ</h1>
+                        <div className="open-letter">THƯ NGỎ</div>
                         <h1>Quý khách hàng, đối tác thân mến!</h1>
                         <p>Việt Chuyên SG là Nhà Cung cấp các giải pháp & sản phẩm cơ sở hạ tầng hệ thống CNTT tại Việt Nam.Với kinh nghiệm hơn 10 năm trong ngành Công nghệ thông tin tại Việt Nam, Việt Chuyên SG luôn cam kết làm việc theo phương châm “Chất lượng- Uy tín – Chuyên nghiệp – Sáng tạo – Nỗ lực không ngừng”, từ đó đề xuất những phương án hướng tới sự phát triển lâu dài, cung cấp tới các khách hàng những sản phẩm, dịch vụ CNTT tốt nhất. Chính vì thế, từ thương hiệu ít được biết đến, Viet Chuyen SG đã xây dựng tên tuổi thành công và tạo được dấu ấn rất riêng.</p>
                         <p>“Thành công đến từ sự chân thành là thành công bền vững nhất” và không tài sản nào giá trị hơn việc khách hàng biết đến thương hiệu của mình.Có được những thành quả đó không thể không nhắc đến sự ủng hộ và tin tưởng của Quý khách hàng, đối tác.</p>
