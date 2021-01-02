4cf86d4b2053270b6671a1fdf63b5a0d486a86e3
TruHuynh_DXC <hctru68@gmail.com>
2021-01-02 19:10:27 +0700

    update scripts

--- src/features/about/index.jsx
+++ src/features/about/index.jsx
@@ -8,9 +8,9 @@ const About = () => {
 
     return (
         <Row>
-            <Col lg={12} className="about">
+            <Col xs={12} className="about">
                 <Row className="text-about">
-                    <Col lg={{ span: 6, offset: 2 }}>
+                    <Col xs={{ span: 6, offset: 2 }}>
                         <div className="open-letter">THƯ NGỎ</div>
                         <h1>Quý khách hàng, đối tác thân mến!</h1>
                         <p>Việt Chuyên SG là Nhà Cung cấp các giải pháp & sản phẩm cơ sở hạ tầng hệ thống CNTT tại Việt Nam.Với kinh nghiệm hơn 10 năm trong ngành Công nghệ thông tin tại Việt Nam, Việt Chuyên SG luôn cam kết làm việc theo phương châm “Chất lượng- Uy tín – Chuyên nghiệp – Sáng tạo – Nỗ lực không ngừng”, từ đó đề xuất những phương án hướng tới sự phát triển lâu dài, cung cấp tới các khách hàng những sản phẩm, dịch vụ CNTT tốt nhất. Chính vì thế, từ thương hiệu ít được biết đến, Viet Chuyen SG đã xây dựng tên tuổi thành công và tạo được dấu ấn rất riêng.</p>
@@ -22,10 +22,10 @@ const About = () => {
                 </Row>
                 <div className="contain-about">
                     <Row>
-                        <Col lg={{ span: 4, offset: 2 }}>
+                        <Col xs={{ span: 4, offset: 2 }}>
                             <img src={IMAGES.MISSION} width="577" alt="mission" />
                         </Col>
-                        <Col lg={{ span: 3, offset: 1 }}>
+                        <Col xs={{ span: 3, offset: 1 }}>
                             <div className="title-about blue">SỨ MỆNH</div>
                             <div className="detail-about">
                                 Không ngừng đổi mới và sáng tạo để khẳng định sự khác biệt, đồng thời trân trọng và đề cao con người, bồi dưỡng nguồn nhân lực nhằm đáp ứng ngày một tốt hơn những yêu cầu của khách hàng, những biến chuyển của thị trường, nâng cao giá trị sản phẩm dịch vụ.
@@ -33,34 +33,80 @@ const About = () => {
                         </Col>
                     </Row>
                     <Row>
-                        <Col lg={{ span: 3, offset: 2 }}>
+                        <Col xs={{ span: 3, offset: 2 }}>
                             <div className="title-about red">TẦM NHÌN</div>
                             <div className="detail-about">
                                 Không ngừng đổi mới và sáng tạo để khẳng định sự khác biệt, đồng thời trân trọng và đề cao con người, bồi dưỡng nguồn nhân lực nhằm đáp ứng ngày một tốt hơn những yêu cầu của khách hàng, những biến chuyển của thị trường, nâng cao giá trị sản phẩm dịch vụ.
                             </div>
                         </Col>
-                        <Col lg={{ span: 6, offset: 1 }}>
+                        <Col xs={{ span: 6, offset: 1 }}>
                             <img src={IMAGES.VISION} width="577" alt="vision" />
                         </Col>
                     </Row>
                     <Row>
-                        <Col lg={{ span: 4, offset: 2 }}>
+                        <Col xs={{ span: 4, offset: 2 }}>
                             <img src={IMAGES.CORE_VALUE} width="577" alt="core-value" />
                         </Col>
-                        <Col lg={{ span: 3, offset: 1 }}>
+                        <Col xs={{ span: 3, offset: 1 }}>
                             <div className="title-about blue">GIÁ TRỊ CỐT LỖI</div>
                             <div className="detail-about">
                                 Hơn 10 năm qua, đội ngũ cán bộ nhân viên Viet Chuyen SG luôn cam kết làm việc theo phương châm:
-                        </div>
+                            </div>
                             <div className="title-small-about red">“Chất lượng - Uy tín – Chuyên nghiệp – Sáng tạo – Nỗ lực không ngừng”</div>
                             <div className="detail-about">
                                 Luôn luôn đề xuất những phương án hướng tới sự phát triển lâu dài nhằm cung cấp các sản phẩm, dịch vụ CNTT chất lượng cao đến các khách hàng.
-                        </div>
+                            </div>
                         </Col>
                     </Row>
                 </div>
+                <Row className="site-map">
+                    <Col xs={12} className="text-center">
+                        {/* <svg className="box-cctc">
+                            <polygon points="0,400 100,300 700,300 1000,0 1000,1900 800,2100 250,2100 0,2333"
+                                style={{ stroke: 'black', 'strokeWidth': 1, 'fill': '#008CF8' }} />
+                                Sorry, your browser does not support inline SVG.
+                        </svg> */}
+                        <div className="open-letter">Cơ cấu tổ chức</div>
+                        <p>Cơ cấu tổ chức của Việt Chuyên SG được chia thành các nhóm chức năng chuyên môn,<br />nhằm phục vụ tốt nhất cho nhu cầu của khách hàng.</p>
+                        <br /><br />
+                        <div className="tree-map mgBottom80">
+                            <Row>
+                                <Col md={{ span: 8, offset: 4 }}>
+                                    <div className="node white-node txt-uppercase"><p>Hội đồng thành viên</p></div>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={{ span: 3, offset: 4 }}>
+                                    <div className="node white-node txt-uppercase"><p>Giám đốc điều hành</p></div>
+                                </Col>
+                                <Col md={5}>
+                                    <div className="node light-green-node txt-uppercase"><p>P. Nghiên cứu & Phát triển</p></div>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={{ span: 3, offset: 1 }}>
+                                    <div className="node blue-node txt-uppercase"><p>Phó GĐ Kinh doanh</p></div>
+                                    <div className="node blue-node"><p>Phòng Dự án</p></div>
+                                    <div className="node blue-node"><p>Phòng Marketing</p></div>
+                                    <div className="node blue-node"><p>Phòng Kinh doanh</p></div>
+                                </Col>
+                                <Col md={{ span: 3, offset: 0 }}>
+                                    <div className="node yellow-node txt-uppercase"><p>Phó GĐ Điều hành</p></div>
+                                    <div className="node yellow-node"><p className="multiple-line">P. Kỹ thuật lắp đặt<br />và dịch vụ hậu mãi</p></div>
+                                    <div className="node yellow-node"><p>P. Hành chính & Nhân sự</p></div>
+                                </Col>
+                                <Col md={{ span: 5, offset: 0 }}>
+                                    <div className="node red-node txt-uppercase"><p>TP. Kế toán - Tài chính</p></div>
+                                    <div className="node red-node"><p>Kế toán tổng hợp</p></div>
+                                    <div className="node red-node"><p>Kế toán thanh toán</p></div>
+                                    <div className="node red-node"><p>Kế toán vật tư</p></div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
             </Col>
-        </Row>
+        </Row >
     );
 }
 export default About
