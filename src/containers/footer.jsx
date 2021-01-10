ab334b4b7d2cdefcd6f234121c07ae37c67358e0
DONG <dongndt2@gmail.com>
2021-01-10 11:54:41 +0700

    fix VCP 19 21

--- src/containers/footer.jsx
+++ src/containers/footer.jsx
@@ -69,7 +69,7 @@ const TheFooter = () => {
                 </Container>
             </Figure>
 
-            <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62712.096755965526!2d106.63502353718317!3d10.772500054351118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ede0aa80245%3A0x2b6b5c4999a448ce!2zNzQwIFPGsCBW4bqhbiBI4bqhbmgsIFBoxrDhu51uZyAxMiwgUXXhuq1uIDEwLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1607483130626!5m2!1sen!2s"
+            <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.23696364562!2d106.62428871406844!3d10.793154492310165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752c007f436d81%3A0x4b4529747ba52d36!2zQ2h1bmcgQ8awIMSQ4buZYyBM4bqtcCBC!5e0!3m2!1svi!2s!4v1610253207388!5m2!1svi!2s"
                 frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0" title="map"></iframe>
         </footer>
     );
